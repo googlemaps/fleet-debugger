527e23ad7adcd3184c9f2cf8e44bf21dc0abeb73
greghutch <4603341+greghutch@users.noreply.github.com>
2021-12-14 16:16:25 -0800

    fix: add links to documentation for the visualizations
    Also added placeholder doc pages for jump visualizations
    
    Fix: 80

--- src/ToggleBar.js
+++ src/ToggleBar.js
@@ -15,10 +15,26 @@ const ButtonToggle = styled(Button)`
   `}
 `;
 
+const docLinks = {
+  showGPSBubbles:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/GPSAccuracy.md",
+  showHeading:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/Heading.md",
+  showSpeed:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/Speed.md",
+  showDwellLocations:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/DwellTimes.md",
+  showHighVelocityJumps:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/VelocityJumps.md",
+  showMissingUpdates:
+    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/MissingUpdates.md",
+};
+
 const ButtonGroup = styled.div`
   display: flex;
 `;
 function ToggleBar(props) {
+  // TODO: learn JSX ... make this a for loop
   return (
     <ButtonGroup>
       <ButtonToggle
@@ -26,30 +42,52 @@ function ToggleBar(props) {
         onClick={props.onClickGPSBubbles}
       >
         GPS Accuracy
+        <a href={docLinks.showGPSBubbles} target="_blank" rel="noreferrer">
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle active={props.showHeading} onClick={props.onClickHeading}>
         Heading
+        <a href={docLinks.showHeading} target="_blank" rel="noreferrer">
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle active={props.showSpeed} onClick={props.onClickSpeed}>
         Speed
+        <a href={docLinks.showSpeed} target="_blank" rel="noreferrer">
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle
         active={props.showDwellLocations}
         onClick={props.onClickDwellLocations}
       >
         Dwell Locations
+        <a href={docLinks.showDwellLocations} target="_blank" rel="noreferrer">
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle
         active={props.showHighVelocityJumps}
         onClick={props.onClickHighVelocityJumps}
       >
         Jumps (unrealistic velocity)
+        <a
+          href={docLinks.showHighVelocityJumps}
+          target="_blank"
+          rel="noreferrer"
+        >
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle
         active={props.showMissingUpdates}
         onClick={props.onClickMissingUpdates}
       >
         Jumps (Temporal)
+        <a href={docLinks.showMissingUpdates} target="_blank" rel="noreferrer">
+          ?
+        </a>
       </ButtonToggle>
       <ButtonToggle active={props.showTraffic} onClick={props.onClickTraffic}>
         Traffic
