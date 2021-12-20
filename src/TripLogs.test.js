b3ecb92e6f3b9a3b2938592592d80b9a2a90944b
Greg Hutchins <4603341+greghutch@users.noreply.github.com>
2021-12-20 09:05:02 -0800

    fix: add first regression test (#84)
    Only the first of many.   Ensure trip loading returns the expected
    set of trips for both odrd & lmfs use cases
    
    Other changes:
      - moved more of the logic out of vehicleData into TripLogs.  Didn't
        attempt to remove some likely redundant work (ie the _.reverse()
              call)
    
    Fix: #38

---
+++ src/TripLogs.test.js
@@ -0,0 +1,40 @@
+import TripLogs from "./TripLogs";
+
+async function loadTripLogs(dataset) {
+  const module = await import(dataset);
+  const parsedData = module.parsedData;
+  return new TripLogs(parsedData.rawLogs, parsedData.solutionType);
+}
+
+test("basic odrd trip log loading", async () => {
+  const tripLogs = await loadTripLogs("../datasets/jump-demo.js");
+
+  expect(tripLogs.getTripIDs()).toStrictEqual([
+    "non-trip-segment-0",
+    "a51c8f15-47c0-4bae-8262-8421b0a3e058",
+    "non-trip-segment-1",
+    "2f03ff03-1d0e-49e2-91d4-f3c674d98073",
+    "non-trip-segment-2",
+    "d9d7686f-0dd2-4114-a1e2-ae11201d4c0f",
+    "non-trip-segment-3",
+    "73a320f8-0e1f-44cb-ba99-cdad010cdaf4",
+    "non-trip-segment-4",
+  ]);
+});
+
+test("basic lmfs trip log loading", async () => {
+  const tripLogs = await loadTripLogs("../datasets/lmfs.js");
+
+  expect(tripLogs.getTripIDs()).toStrictEqual([
+    "Stops Left 10",
+    "Stops Left 9",
+    "Stops Left 8",
+    "Stops Left 7",
+    "Stops Left 6",
+    "Stops Left 5",
+    "Stops Left 4",
+    "Stops Left 3",
+    "Stops Left 2",
+    "Stops Left 1",
+  ]);
+});
--- src/vehicleData.js
+++ src/vehicleData.js
@@ -1,27 +1,18 @@
 /*
  * vehicleData.js
  *
- * Processes raw log data for easier consumption by other components.
+ * Load raw log data for easier consumption by other components.
  * In theory this could be done serverside & placed into rawData, but
  * it's easier to iterate on features when the raw log data doesn't need
  * to be regenerated each time
  */
-import _ from "lodash";
 import { parsedData } from "./rawData";
-const rawLogs = _.reverse(parsedData.rawLogs);
+const rawLogs = parsedData.rawLogs;
 const jwt = parsedData.jwt;
 const projectId = parsedData.projectId;
 const apikey = parsedData.APIKEY;
 const solutionType = parsedData.solutionType;
 import TripLogs from "./TripLogs";
-
-//  annotate with Dates & timestapms
-_.map(rawLogs, (le, idx) => {
-  le.date = new Date(le.timestamp);
-  le.formattedDate = le.date.toISOString();
-  le.timestampMS = le.date.getTime();
-  le.idx = idx;
-});
 
 const tripLogs = new TripLogs(rawLogs, solutionType);
 export { tripLogs, apikey, jwt, projectId, solutionType };
