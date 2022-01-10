8c65545305e104a9c31e01692b369e59c876f10b
greghutch <4603341+greghutch@users.noreply.github.com>
2022-01-10 14:52:23 -0800

    fix: add support saving/restoring state from the url
    With this change the state of the visualizations as well
    as the map zoom and time range will be saved & restored from
    the url.  Links now work as expected!
    
    Other changes:
      - reduced some of the duplication/boilerplate for toggle
        buttons

--- src/ToggleBar.js
+++ src/ToggleBar.js
@@ -4,6 +4,7 @@
  * Row of buttons that configure visualization options on the map
  */
 import styled from "styled-components";
+import _ from "lodash";
 const Button = styled.button``;
 const ButtonToggle = styled(Button)`
   opacity: 0.6;
@@ -15,94 +16,26 @@ const ButtonToggle = styled(Button)`
   `}
 `;
 
-const docLinks = {
-  showGPSBubbles:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/GPSAccuracy.md",
-  showHeading:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/Heading.md",
-  showSpeed:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/Speed.md",
-  showDwellLocations:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/DwellTimes.md",
-  showHighVelocityJumps:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/VelocityJumps.md",
-  showMissingUpdates:
-    "https://github.com/googlemaps/fleet-debugger/blob/main/docs/MissingUpdates.md",
-};
-
 const ButtonGroup = styled.div`
   display: flex;
 `;
 function ToggleBar(props) {
-  // TODO: learn JSX ... make this a for loop
-  return (
-    <ButtonGroup>
+  const toggleState = props.toggleState;
+  const toggles = _.map(props.toggles, (toggle) => {
+    return (
       <ButtonToggle
-        active={props.showGPSBubbles}
-        onClick={props.onClickGPSBubbles}
+        key={toggle.id}
+        active={toggleState[toggle.id]}
+        onClick={() => props.clickHandler(toggle.id)}
       >
-        GPS Accuracy
-        <a href={docLinks.showGPSBubbles} target="_blank" rel="noreferrer">
+        {toggle.name}
+        <a href={toggle.docLink} target="_blank" rel="noreferrer">
           ?
         </a>
       </ButtonToggle>
-      <ButtonToggle active={props.showHeading} onClick={props.onClickHeading}>
-        Heading
-        <a href={docLinks.showHeading} target="_blank" rel="noreferrer">
-          ?
-        </a>
-      </ButtonToggle>
-      <ButtonToggle active={props.showSpeed} onClick={props.onClickSpeed}>
-        Speed
-        <a href={docLinks.showSpeed} target="_blank" rel="noreferrer">
-          ?
-        </a>
-      </ButtonToggle>
-      <ButtonToggle
-        active={props.showDwellLocations}
-        onClick={props.onClickDwellLocations}
-      >
-        Dwell Locations
-        <a href={docLinks.showDwellLocations} target="_blank" rel="noreferrer">
-          ?
-        </a>
-      </ButtonToggle>
-      <ButtonToggle
-        active={props.showHighVelocityJumps}
-        onClick={props.onClickHighVelocityJumps}
-      >
-        Jumps (unrealistic velocity)
-        <a
-          href={docLinks.showHighVelocityJumps}
-          target="_blank"
-          rel="noreferrer"
-        >
-          ?
-        </a>
-      </ButtonToggle>
-      <ButtonToggle
-        active={props.showMissingUpdates}
-        onClick={props.onClickMissingUpdates}
-      >
-        Jumps (Temporal)
-        <a href={docLinks.showMissingUpdates} target="_blank" rel="noreferrer">
-          ?
-        </a>
-      </ButtonToggle>
-      <ButtonToggle
-        active={props.showClientServerTimeDeltas}
-        onClick={props.onClickClientServerTimeDeltas}
-      >
-        Client/Server Time Deltas
-      </ButtonToggle>
-      <ButtonToggle active={props.showTraffic} onClick={props.onClickTraffic}>
-        Traffic
-      </ButtonToggle>
-      <ButtonToggle active={props.showLiveJS} onClick={props.onClickLiveJS}>
-        Start Live Journey Sharing for newest trip
-      </ButtonToggle>
-    </ButtonGroup>
-  );
+    );
+  });
+  return <ButtonGroup>{toggles}</ButtonGroup>;
 }
 
 export default ToggleBar;
