dac7a5f6f3a9d660f45149beb6c30bbea0402acb
Crystal Zhao <crystalzhao@google.com>
2022-07-30 15:33:33 -0700

    add pagination to speed up UI

--- src/LogTable.js
+++ src/LogTable.js
@@ -3,7 +3,7 @@
  *
  * Handles the log viewing component.
  */
-import { useFilters, useSortBy, useTable } from "react-table";
+import { useFilters, usePagination, useSortBy, useTable } from "react-table";
 import React, { useState } from "react";
 import styled from "styled-components";
 import _ from "lodash";
@@ -88,68 +88,134 @@ function Table({ columns, data, onSelectionChange }) {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-        defaultColumn,
-        filterTypes,
-        autoResetFilters: false,
-        autoResetSortBy: false,
-      },
-      useFilters,
-      useSortBy
-    );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      filterTypes,
+      autoResetFilters: false,
+      autoResetSortBy: false,
+      autoResetPage: false,
+    },
+    useFilters,
+    useSortBy,
+    usePagination
+  );
+
+  const { pageIndex, pageSize } = state;
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>
-                <div
-                  className="logtable-head"
-                  {...column.getSortByToggleProps()}
-                >
-                  {column.render("Header")}
-                  <div style={{ width: 20 }}>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? " 🔽"
-                        : " 🔼"
-                      : ""}
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  <div
+                    className="logtable-head"
+                    {...column.getSortByToggleProps()}
+                  >
+                    {column.render("Header")}
+                    <div style={{ width: 20 }}>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " 🔽"
+                          : " 🔼"
+                        : ""}
+                    </div>
                   </div>
-                </div>
-                <div>{column.canFilter ? column.render("Filter") : null}</div>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr
-              {...row.getRowProps()}
-              className={`logtable-row ${
-                selectedRow === row.index ? "selected" : ""
-              }`}
-              onClick={() => {
-                setSelectedRow(row.index);
-                onSelectionChange(row.original);
-              }}
-            >
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                className={`logtable-row ${
+                  selectedRow === row.index ? "selected" : ""
+                }`}
+                onClick={() => {
+                  setSelectedRow(row.index);
+                  onSelectionChange(row.original);
+                }}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
