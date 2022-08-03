317cfba3daccb9481972395c6022a2889b2be34d
Crystal Zhao <106219868+crystalzha0@users.noreply.github.com>
2022-08-03 13:39:55 -0700

    add sort, filter, pagination and highlight selection to log table (#142)
    * add sort, filter and highlight selection to log table
    
    * add pagination to speed up UI

--- src/LogTable.js
+++ src/LogTable.js
@@ -3,8 +3,8 @@
  *
  * Handles the log viewing component.
  */
-import { useTable } from "react-table";
-import React from "react";
+import { useFilters, usePagination, useSortBy, useTable } from "react-table";
+import React, { useState } from "react";
 import styled from "styled-components";
 import _ from "lodash";
 
@@ -35,42 +35,187 @@ const Styles = styled.div`
       }
     }
   }
+
+  .logtable-head {
+    display: flex;
+  }
+  .logtable-row:hover {
+    background-color: #e6e6e6;
+  }
+  .logtable-row.selected {
+    background-color: #d0d0ff;
+  }
 `;
 
 function Table({ columns, data, onSelectionChange }) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
+  const [selectedRow, setSelectedRow] = useState(-1);
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  );
+
+  function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length;
+
+    return (
+      <input
+        value={filterValue || ""}
+        onChange={(e) => {
+          setFilter(e.target.value || undefined);
+        }}
+        placeholder={`Search ${count} records...`}
+      />
+    );
+  }
+
+  const filterTypes = React.useMemo(
+    () => ({
+      text: (rows, id, filterValue) => {
+        return rows.filter((row) => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      },
+    }),
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    prepareRow,
+  } = useTable(
+    {
       columns,
       data,
-    });
+      defaultColumn,
+      filterTypes,
+      autoResetFilters: false,
+      autoResetSortBy: false,
+      autoResetPage: false,
+    },
+    useFilters,
+    useSortBy,
+    usePagination
+  );
+
+  const { pageIndex, pageSize } = state;
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr
-              {...row.getRowProps()}
-              onClick={() => onSelectionChange(row.original)}
-            >
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  <div
+                    className="logtable-head"
+                    {...column.getSortByToggleProps()}
+                  >
+                    {column.render("Header")}
+                    <div style={{ width: 20 }}>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " 🔽"
+                          : " 🔼"
+                        : ""}
+                    </div>
+                  </div>
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                className={`logtable-row ${
+                  selectedRow === row.index ? "selected" : ""
+                }`}
+                onClick={() => {
+                  setSelectedRow(row.index);
+                  onSelectionChange(row.original);
+                }}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
